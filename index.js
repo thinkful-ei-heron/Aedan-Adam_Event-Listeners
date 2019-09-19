6b2d548398f6d5d235640b699a498d4985bf8b44
Just-A-Fool <aedanscarn@gmail.com>
2019-09-19 11:38:23 -0600

    working

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
-$('.thumbnail').on('click', e => {
-    let targetCat = $(e.target);
-    let untargetCat = $('.thumnail').not(targetCat);
+$('.thumbnail').on('click',  e => {
+    let targetCat = $(e.currentTarget).find('img');
+    let untargetCat = $('.js-cat');
 
-    $('.')
+    untargetCat.attr('aria-pressed', false);
+    targetCat.attr('aria-pressed', true);
 
     $('.hero').find('img').attr('src', targetCat.attr('src'));
     $('.hero').find('img').attr('alt', targetCat.attr('alt'));
 
 
-    // console.log(e.target);
+    console.log(untargetCat);
     console.log($(e.target).attr('src'));
   });
   
